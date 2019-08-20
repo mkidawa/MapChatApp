87c9aaec1793df7413fb4a70835cfc2675cac662
Michael Kidawa <mkidawa1@gmail.com>
2019-08-20 15:08:00 +0200

    Notifications css fixed

--- client-simple/src/components/Chat/ChatMessages.js
+++ client-simple/src/components/Chat/ChatMessages.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import gql from "graphql-tag";
-import { graphql, compose } from "react-apollo";
+import {compose, graphql} from "react-apollo";
 import _ from "lodash";
-import { Message, MessageText } from "../Message/Message";
+import {Message, MessageText} from "../Message/Message";
 import withSocket from "../withSocket";
 import withUserContext from "../withUserContext";
 import {NotificationContainer, NotificationManager} from "react-notifications";
+import "react-notifications/lib/notifications.css";
 
 class ChatMessages extends Component {
   state = {
@@ -17,8 +18,8 @@ class ChatMessages extends Component {
   loggedUserId = () => _.get(this.props, ["context", "userState", "user", "id"], null);
 
   componentDidMount = async () => {
-    const { socket } = this.props;
-    
+    const {socket} = this.props;
+
     socket.on("message", msg => this.handleIncomingMessage(msg));
   };
 
@@ -42,37 +43,40 @@ class ChatMessages extends Component {
 
       NotificationManager.success(checkedMessage, msg.from.nickname, 10000);
     }
+
   };
 
   renderMessage = (message) => {
-    const {guestId } = this.state;
+    const {guestId} = this.state;
 
     const getMsgAuthorNickname = () => {
       const _socketIoNickname = message.from && message.from.nickname;
-      const _graphQlNickname = _.get(message, ["from", "profile", "firstName"], null);
+      const _graphQlNickname = _.get(message, ["from", "profile"], null);
       const {guestName} = message;
 
-      return _socketIoNickname || _graphQlNickname || guestName || "Unknown User";
+      return _socketIoNickname || _graphQlNickname && _graphQlNickname.firstName + " " + _graphQlNickname.lastName || guestName || "Unknown User";
     };
 
     const isMsgOfMine = (message.from && message.from._id || message.guestId) === (this.loggedUserId() || guestId);
 
     return (
       <Message key={message._id} author={getMsgAuthorNickname()} toRight={isMsgOfMine} timestamp={message.createdAt}>
-        <MessageText variant={(isMsgOfMine) ? "primary" : ""}>{message.msg}</MessageText>
+        <MessageText chosenLink={this.props.chosenLink} variant={(isMsgOfMine) ? "primary" : ""}>{message.msg}</MessageText>
       </Message>
     );
   };
 
   render() {
-    const { messages } = this.state;
+    const {messages} = this.state;
     const {previousMessages = []} = this.props;
     const newPreviousAndNewMessages = [...previousMessages, ...messages];
 
     return (
       <>
         {newPreviousAndNewMessages.map((message) => this.renderMessage(message))}
-        <div ref={el => {this.scrollToBottomElement = el; }} />
+        <div ref={el => {
+          this.scrollToBottomElement = el;
+        }}/>
         <NotificationContainer/>
       </>
     );
@@ -99,8 +103,8 @@ const GET_PREVIOUS_MESSAGES = gql`
 `;
 
 const withPreviousMessages = graphql(GET_PREVIOUS_MESSAGES, {
-  options: (props) => ({ variables: { chatroom: props.match.params.chatId }}),
-  props: ({ data }) => ({previousMessages: data.messages})
+  options: (props) => ({variables: {chatroom: props.match.params.chatId}}),
+  props: ({data}) => ({previousMessages: data.messages})
 });
 
 export default compose(withPreviousMessages)(withSocket(withUserContext(ChatMessages)));
